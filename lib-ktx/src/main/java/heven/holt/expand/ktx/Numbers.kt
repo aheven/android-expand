8679a12f627f87b91e8fb54798cf1ee5ad776f9b
Heven Holt <hyt0302@qq.com>
2023-05-09 10:55:41 +0800

    add ktx new lib model

---
+++ lib-ktx/src/main/java/heven/holt/expand/ktx/Numbers.kt
@@ -0,0 +1,29 @@
+package heven.holt.expand.ktx
+
+fun Float.format1f(): String {
+    return String.format("%.1f", this)
+}
+
+fun Float.format2f(): String {
+    return String.format("%.2f", this)
+}
+
+fun Float.format3f(): String {
+    return String.format("%.3f", this)
+}
+
+fun Double.format1f(): String {
+    return String.format("%.1f", this)
+}
+
+fun Double.format2f(): String {
+    return String.format("%.2f", this)
+}
+
+fun Double.format3f(): String {
+    return String.format("%.3f", this)
+}
+
+fun Int.toBoolean(): Boolean {
+    return this != 0
+}
